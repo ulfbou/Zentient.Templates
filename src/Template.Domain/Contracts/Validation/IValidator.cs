e3ee88629d9702dc88f72f18da205b84115d8147
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-05-15 11:55:25 +0200

    feat: Introduce validation contracts
    This commit defines two new interfaces within the `Template.Domain.Contracts.Validation` namespace to establish a clear contract for implementing validation logic within the domain:
    
    - `IValidator`: A marker interface used to identify all validator implementations.
    - `IValidator<TEntity, TKey>`: Defines the contract for validating entities of type `TEntity`. Implementations of this interface must provide a `Validate` method that takes an entity as input and throws a `DomainValidationException` if the entity is found to be invalid. The `TKey` parameter is included for potential context but is not strictly used in the current definition.
    
    These interfaces promote a consistent approach to validation across the domain, making it easier to implement and integrate validation rules for different entities.

---
+++ src/Template.Domain/Contracts/Validation/IValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace Template.Domain.Contracts.Validation
+{
+    /// <summary>
+    /// Marker interface for all validators.
+    /// </summary>
+    public interface IValidator { }
+}
---
+++ src/Template.Domain/Contracts/Validation/IValidator{in TEntity, in TKey}.cs
@@ -0,0 +1,18 @@
+﻿using Template.Domain.Common.Exceptions;
+
+namespace Template.Domain.Contracts.Validation
+{
+    /// <summary>
+    /// Defines a contract for validating an object.
+    /// </summary>
+    /// <typeparam name="T">The type of object to validate.</typeparam>
+    public interface IValidator<in TEntity, in TKey> : IValidator
+    {
+        /// <summary>
+        /// Validates the specified entity. 
+        /// </summary>
+        /// <param name="entity">The object to validate.</param>
+        /// <exception cref="DomainValidationException">Thrown when the object is invalid.</exception>
+        void Validate(TEntity entity);
+    }
+}
