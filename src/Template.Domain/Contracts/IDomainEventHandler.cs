f5fbcf10f3cf78a4f0a512d6bd2c79b18abcc558
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-05-15 15:03:43 +0200

    feat: Rename notification parameter in IDomainEventHandler
    This commit renames the `notification` parameter in the `Handle` method of the `IDomainEventHandler<in TEvent>` interface to `@event`.
    
    This change aims to improve code readability and avoid potential naming conflicts with the `Event` suffix that is commonly used for domain event classes. The `@` symbol is used as a prefix to the parameter name to allow using the reserved keyword `event` without causing a compiler error.
    
    The updated parameter name more clearly reflects the purpose of the parameter, which is to receive the domain event being handled.

--- src/Template.Domain/Contracts/IDomainEventHandler.cs
+++ src/Template.Domain/Contracts/IDomainEventHandler.cs
@@ -3,15 +3,15 @@
     /// <summary>
     /// Interface for handling domain events.
     /// </summary>
-    /// typeparam name="TEvent">The type of the domain event.</typeparam>
+    /// <typeparam name="TEvent">The type of the domain event.</typeparam>
     public interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
     {
         /// <summary>
         /// Handles the domain event.
         /// </summary>
-        /// <param name="notification">The domain event to handle.</param>
+        /// <param name="event">The domain event to handle.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        Task Handle(TEvent notification, CancellationToken cancellationToken);
+        Task Handle(TEvent @event, CancellationToken cancellationToken);
     }
 }
