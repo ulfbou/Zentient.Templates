4dff110c1398e6242a3acc5c93198ad142aa8052
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-05-23 09:00:16 +0200

    feat: Update IIdempotencyStore to use Zentient.Results
    This commit updates the `IIdempotencyStore` interface to align with the new result handling mechanism using the `Zentient.Results` library.
    
    **Key change:**
    
    -   The `StoreResultAsync` method's generic constraint is updated from `where TResponse : IResult` (the removed internal `IResult` interface) to `where TResponse : Zentient.Results.IResult`.
    
    This ensures that the idempotency store correctly handles and stores results that are consistent with the application's current result handling strategy using `Zentient.Results`. Any implementations of `IIdempotencyStore` will need to be updated accordingly to work with the `Zentient.Results.IResult` interface.

--- src/Template.Application/Common/Contracts/IIdempotencyStore.cs
+++ src/Template.Application/Common/Contracts/IIdempotencyStore.cs
@@ -1,6 +1,8 @@
 ﻿using Template.Domain.Common.Result;
 using Template.Domain.ValueObjects;
 
+using Zentient.Results;
+
 namespace Template.Application.Common.Contracts
 {
     public interface IIdempotencyStore
