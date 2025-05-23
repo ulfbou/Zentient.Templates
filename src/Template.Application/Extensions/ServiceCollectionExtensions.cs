91e7addc75b9c4c665bade0553bd4fedb246aa82
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-05-23 08:19:37 +0200

    feat: Register application-specific contexts in DI container
    This commit introduces an extension method `AddContexts` for `IServiceCollection` within the `Template.Application.Extensions` namespace. This method is responsible for registering application-specific context services into the dependency injection (DI) container.
    
    Specifically, it registers the following services with a scoped lifetime:
    
    -   `IUserContext`: Implemented by `UserContext`. This context likely provides access to information about the current user.
    -   `IRequestContext`: Implemented by `RequestContext`. This context likely provides access to information about the current HTTP request.
    
    By registering these contexts as scoped, a new instance of each context will be created for each HTTP request within the application's scope, ensuring that context-specific information is properly managed throughout the request lifecycle. This extension method simplifies the configuration of these essential application services.

---
+++ src/Template.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+
+using Template.Application.Common.Contexts;
+using Template.Application.Common.Contracts;
+
+namespace Template.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddContexts(this IServiceCollection services)
+        {
+            services.AddScoped<IUserContext, UserContext>();
+            services.AddScoped<IRequestContext, RequestContext>();
+
+            return services;
+        }
+    }
+}
